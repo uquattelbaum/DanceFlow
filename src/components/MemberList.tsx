d26494ac80db8f0d5edcf95df4151c9ce0be752f
Udo Quattelbaum <u.quattelbaum@outlook.com>
2025-08-04 15:57:30 +0200

    Erster Deploy-Test

--- src/components/MemberList.tsx
+++ src/components/MemberList.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; 
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY; 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 interface Member {
